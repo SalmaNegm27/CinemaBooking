dd67f2bdd7e9028ec38747987a3fca34e64f29e7
salma.negm <salma.negm@W10-E114-SOFT4>
2023-07-19 15:33:04 +0300

    DeleteMovie

---
+++ CinemaBooking/Controllers/SecurityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaBooking.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
